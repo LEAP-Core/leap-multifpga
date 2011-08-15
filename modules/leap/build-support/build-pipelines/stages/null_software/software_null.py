1ed81d4401710d515ce56169d62d96b102b64a80
Elliott Fleming <kermin.fleming@intel.com>
2011-08-15 22:22:45 +0000

    CSN: CSN-multifpga-47
    need fix for syntax.h.

--- modules/leap/build-support/build-pipelines/stages/null_software/software_null.py
+++ modules/leap/build-support/build-pipelines/stages/null_software/software_null.py
@@ -4,6 +4,22 @@ import re
 import SCons.Script
 from model import  *
 
+def host_defs():
+    hostos = one_line_cmd('uname -s')
+    hostmachine = one_line_cmd('uname -m')
+
+    if (hostos == 'FreeBSD'):
+        hflags = '-DHOST_FREEBSD'
+    else:
+        hflags = '-DHOST_LINUX'
+        if (hostmachine == 'ia64'):
+            hflags += ' -DHOST_LINUX_IA64'
+        else:
+            hflags += ' -DHOST_LINUX_X86'
+
+    return hflags
+
+
 class Software():
 
   def __init__(self, moduleList):
