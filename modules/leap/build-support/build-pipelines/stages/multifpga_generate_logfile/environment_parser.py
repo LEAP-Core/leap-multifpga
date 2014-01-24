cddc5fb1c0b6269ca95342563ae9e344a3dc994c
Elliott Fleming <kermin.fleming@intel.com>
2014-01-24 09:52:59 +0000

    CSN: CSN-multifpga-214
    Reorganizing files to improve code readability.

--- modules/leap/build-support/build-pipelines/stages/multifpga_generate_logfile/environment_parser.py
+++ modules/leap/build-support/build-pipelines/stages/multifpga_generate_logfile/environment_parser.py
@@ -1,7 +1,7 @@
 import ply.yacc as yacc
 import ply.lex as lex
-from multilex import *
-from multiparse import *
+from environment_lex import *
+from environment_parse import *
 from environment import *
 
 def parseFPGAEnvironment (environmentFile):
