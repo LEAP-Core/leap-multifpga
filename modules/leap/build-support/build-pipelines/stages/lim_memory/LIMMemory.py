d54ddeccdda3aab4673a21942e97429d05cc8a23
Hsin-Jung Yang <hjyang@mit.edu>
2016-08-01 11:22:55 -0400

    Add a switch to interleave all scratchpad clients across controller networks. (This is useful for simulate remapping options.)

--- modules/leap/build-support/build-pipelines/stages/lim_memory/LIMMemory.py
+++ modules/leap/build-support/build-pipelines/stages/lim_memory/LIMMemory.py
@@ -89,5 +89,6 @@ class LIMMemory():
         K_ARY = self.moduleList.getAWBParam('lim_memory', 'SCRATCHPAD_REMAP_TREE_K_ARY')
         TREE_MODE = self.moduleList.getAWBParam('lim_memory', 'SCRATCHPAD_REMAP_TREE_MODE')
         DYN_BANDWIDTH = self.moduleList.getAWBParam('lim_memory', 'SCRATCHPAD_DYN_BANDWIDTH_ALLOC_EN')
-        lim_remap_scratchpad.remapScratchpadConnections(self.moduleGraph.modules.values(), self.hwPlatforms, self.scratchpadStats, REMAP_MODE, K_ARY, TREE_MODE, DYN_BANDWIDTH) 
+        INTERLEAVE_ALL = self.moduleList.getAWBParam('lim_memory', 'SCRATCHPAD_REMAP_ALL_INTERLEAVE')
+        lim_remap_scratchpad.remapScratchpadConnections(self.moduleGraph.modules.values(), self.hwPlatforms, self.scratchpadStats, REMAP_MODE, K_ARY, TREE_MODE, DYN_BANDWIDTH, INTERLEAVE_ALL) 
 
